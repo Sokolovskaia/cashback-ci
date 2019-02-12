3a6bd8bf7e7301eeaf3783048ebbf00e15ab7409
Sokolovskaia <sk2m.sokolovskaya@gmail.com>
2019-02-13 00:13:34 +0300

    Cashback without limit

---
+++ app/test/cashback.py
@@ -0,0 +1,7 @@
+
+def cashback(amount):
+    percent = 0.05
+    return percent * amount
+
+
+
---
+++ app/test/test_cashback.py
@@ -0,0 +1,8 @@
+from app.test.cashback import cashback
+
+
+def test_cashback_under_limit():
+    result = cashback(1000)
+
+    assert 50 == result
+
