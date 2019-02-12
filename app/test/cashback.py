eb657191f4588705d3dd46dc6a1a5c879b627f87
Sokolovskaia <sk2m.sokolovskaya@gmail.com>
2019-02-13 00:38:04 +0300

    Fix for cashback over limit

--- app/test/cashback.py
+++ app/test/cashback.py
@@ -1,7 +1,13 @@
 
 def cashback(amount):
     percent = 0.05
-    return percent * amount
+    result = percent * amount
+    limit = 3_000
+    if result > limit:
+        return limit
+    return result
+
+
 
 
 
